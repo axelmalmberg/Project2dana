c9bbb0666fd0a1be43498a83bc5bbc4bad3dbc8f
Axel Malmberg <axel_malmberg@hotmail.com>
2015-04-21 18:24:29 +0200

    minor fix

--- src/project2dana/LogInController.java
+++ src/project2dana/LogInController.java
@@ -16,6 +16,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -26,7 +27,10 @@ import javafx.stage.Stage;
 public class LogInController implements Initializable {
 
     @FXML
-    private TextField userNameField, passwordField;
+    private TextField userNameField;
+    
+    @FXML
+    private PasswordField passwordField;
 
     @FXML
     private Button signInButton;
