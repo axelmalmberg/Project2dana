f6ac19446ada83a4d64a7ed5a91a2d2af79f7439
Axel Malmberg <axel_malmberg@hotmail.com>
2015-04-25 19:52:12 +0200

    Made Switch scene possible between all scenes

--- src/project2dana/AddUserController.java
+++ src/project2dana/AddUserController.java
@@ -2,10 +2,16 @@ package project2dana;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -20,6 +26,24 @@ public class AddUserController implements Initializable {
 
     @FXML
     private Button submitButton, returnButton;
+    
+    @FXML
+    private void handleaReturnButtonAction(ActionEvent event) {
+        try {
+            Node node = (Node) event.getSource();
+                    Stage stage = (Stage) node.getScene().getWindow();
+
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("MainMenu.fxml"));
+                    Parent root = loader.load();
+
+                    Scene scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.show();
+        } catch (Exception ex) {
+            
+        }
+        
+    }
 
     /**
      * Initializes the controller class.
--- src/project2dana/MainMenuController.java
+++ src/project2dana/MainMenuController.java
@@ -2,9 +2,15 @@ package project2dana;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -17,6 +23,80 @@ public class MainMenuController implements Initializable {
     @FXML
     private Button addOrdersButton, viewOrdersButton, searchSalesButton, addUserButton, exitButton;
 
+    
+    @FXML
+    private void handleaddOrderButtonAction(ActionEvent event) {
+        try {
+            Node node = (Node) event.getSource();
+                    Stage stage = (Stage) node.getScene().getWindow();
+
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("AddOrders.fxml"));
+                    Parent root = loader.load();
+
+                    Scene scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.show();
+        } catch (Exception ex) {
+            
+        }
+        
+    }
+    
+    @FXML
+    private void handleviewOrderButtonAction(ActionEvent event) {
+        try {
+            Node node = (Node) event.getSource();
+                    Stage stage = (Stage) node.getScene().getWindow();
+
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("ViewOrders.fxml"));
+                    Parent root = loader.load();
+
+                    Scene scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.show();
+        } catch (Exception ex) {
+            
+        }
+        
+    }
+    
+    @FXML
+    private void handleaSearchSalesButtonAction(ActionEvent event) {
+        try {
+            Node node = (Node) event.getSource();
+                    Stage stage = (Stage) node.getScene().getWindow();
+
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("ViewSales.fxml"));
+                    Parent root = loader.load();
+
+                    Scene scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.show();
+        } catch (Exception ex) {
+            
+        }
+        
+    }
+    
+    
+    @FXML
+    private void handleaddUserButtonAction(ActionEvent event) {
+        try {
+            Node node = (Node) event.getSource();
+                    Stage stage = (Stage) node.getScene().getWindow();
+
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("AddUser.fxml"));
+                    Parent root = loader.load();
+
+                    Scene scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.show();
+        } catch (Exception ex) {
+            
+        }
+        
+    }
+    
     /**
      * Initializes the controller class.
      */
